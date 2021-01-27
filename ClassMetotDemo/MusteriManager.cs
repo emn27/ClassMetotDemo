5887a1130206e4eb82af715d87d51230d8c98b78
eminebülbül <emn.bulbul@hotmail.com>
2021-01-27 18:44:32 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + "Bahar isimli musteri eklendi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+                Console.WriteLine("------ Listelenen müsteri-------");
+                Console.WriteLine(" Id " + musteri.Id);
+                Console.WriteLine("TcKimlikNo " + musteri.TcKimlikNo);
+                Console.WriteLine("Adi" + musteri.Adi);
+                Console.WriteLine(" Soyadi " + musteri.Soyadi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Bahar isimli musteri silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.TcKimlikNo = "12345678945";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demirog";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.TcKimlikNo = " 14567896528";
+            musteri2.Adi = "Emine";
+            musteri2.Soyadi = "Bülbül";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.TcKimlikNo = " 65855648962";
+            musteri3.Adi = "Aykut";
+            musteri3.Soyadi = "Öztürk";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteri3);
+
+        }
+    }
+}
